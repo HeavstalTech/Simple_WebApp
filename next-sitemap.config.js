29117292c2a18ecd1e8d13280f0ad3adc351b72e
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-01-02 05:49:30 +0100

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -12,7 +12,7 @@
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://simple-web-app.vercel.app', // REPLACE WITH YOUR DOMAIN
+  siteUrl: process.env.SITE_URL || 'https://simple-webapp-ht.vercel.app', // REPLACE WITH YOUR DOMAIN
   generateRobotsTxt: true, // Generate robots.txt file
   generateIndexSitemap: false, // Set to true for very large sites
   sitemapSize: 7000,
