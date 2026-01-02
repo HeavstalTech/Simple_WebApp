7fc2c913c33aafae2ff42c631c7b80e994b496d6
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-01-02 04:19:17 +0100

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,26 @@
+/* 
+  HEAVSTAL TECH OPEN SOURCE - ROBOTS.TXT
+  --------------------------------------
+  This file dynamically generates the robots.txt file.
+  
+  What it does:
+  - Tells search engine crawlers (Googlebot) which pages they can access.
+  - "Allow: /" means crawl everything.
+  - "Disallow: /private/" would block a folder named private.
+*/
+
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  // Change this to your actual base URL in production
+  const baseUrl = 'https://simple-webapp.vercel.app'; 
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/api/', // Usually, we don't want Google crawling our API endpoints
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
